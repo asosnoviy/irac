ad5a681de2b9448085b08770380c9b42895187f6
Alexey Sosnoviy <int-it@yandex.ru>
2018-03-28 19:39:50 +0300

    sonar

--- travis.sh
+++ travis.sh
@@ -16,15 +16,17 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
         -Dsonar.github.oauth=$SONAR_GITHUB_TOKEN \
         -Dsonar.login=$SONAR_TOKEN \
-        -Dsonar.scanner.skip=false
-        -Dsonar.branch=
+        -Dsonar.scanner.skip=false\
+        -Dsonar.branch=\
+        -Dsonar.branch.name
 
   elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     sonar-scanner \
         -Dsonar.host.url=https://sonar.silverbulleters.org \
         -Dsonar.login=$SONAR_TOKEN \
         -Dsonar.projectVersion=$version\
-        -Dsonar.scanner.skip=false
-        -Dsonar.branch=
+        -Dsonar.scanner.skip=false\
+        -Dsonar.branch=\
+        -Dsonar.branch.name=
   fi
 fi
