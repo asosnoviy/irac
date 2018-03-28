08546714bbdf1d460d2be2b28841b102f1478e0e
Alexey Sosnoviy <int-it@yandex.ru>
2018-03-28 19:57:51 +0300

    Второй заход на ковередж

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 set -e
 
-# oscript /usr/share/oscript/lib/opm/src/opm.os run coverage
+oscript tasks/coverage.os
 
 temp=`cat packagedef | grep ".Версия(" | sed 's|[^"]*"||' | sed -r 's/".+//'`
 version=${temp##*|}
@@ -19,7 +19,7 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
         -Dsonar.scanner.skip=false \
         -Dsonar.branch.name=
         
-        
+
   elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     sonar-scanner \
         -Dsonar.host.url=https://sonar.silverbulleters.org \
